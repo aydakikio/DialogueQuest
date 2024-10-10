2ceec33e59a9973de9b0427c3dab1b8d158da079
aydakikio <kikioayda24@gmail.com>
2024-10-10 23:45:17 +0330

    Version 4.3
    --> Fix: Fixed the choice style's problems

--- Assets/Editor/DialogueQuest/Elements/Basic/Choice_Node.cs
+++ Assets/Editor/DialogueQuest/Elements/Basic/Choice_Node.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
+using System.Linq;
 using DialogueQuest.Data;
 using DialogueQuest.Enumerations;
 using DialogueQuest.Utilities;
+using NUnit.Framework;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -22,7 +25,7 @@ namespace DialogueQuest.Elements
         public override void draw()
         {
             base.draw();
-
+            
             Button Add_Choice = Element_Utilities.Create_Button("Add Choice" , () =>
             {
                 Choice_Data data = new Choice_Data() { Choice_Text = "New Choice" };
@@ -34,13 +37,12 @@ namespace DialogueQuest.Elements
             });
             
             mainContainer.Insert(1 , Add_Choice);
-            
-            foreach(Choice_Data  choice in choices )
+
+            foreach (Choice_Data choice in choices)
             {
                 Port init_choice_port = Create_Choice_Port(choice);
-                
+
                 outputContainer.Add(init_choice_port);
-                
             }
 
             RefreshExpandedState();
@@ -58,17 +60,27 @@ namespace DialogueQuest.Elements
             Button choice_delete_button = Element_Utilities.Create_Button("X", () =>
             {
                 if (choices.Count == 1) return;
-                if (out_put.connected) graph.DeleteElements(out_put.connections);
+                if (out_put.connected)
+                {
+                    //out_put.DisconnectAll();
+                    if (out_put.connections != null)
+                    {
+                        graph.DeleteElements(out_put.connections);
+                    }
+                    
+                }
                 
                 choices.Remove(choice);
                 outputContainer.Remove(out_put);
             });
 
-            TextField choice_text_field = Element_Utilities.Create_TextField(null , choice.Choice_Text,
-                callback => { choice.Choice_Text = callback.newValue; });
+            TextField choice_text_field = Element_Utilities.Create_TextField(null, choice.Choice_Text,callback => { choice.Choice_Text = callback.newValue;});
+
+            choice_text_field.style.minWidth = 20;
+            choice_text_field.style.maxWidth = 80;
             
-            out_put.Add(choice_delete_button);
             out_put.Add(choice_text_field);
+            out_put.Add(choice_delete_button);
             
             return out_put;
         }
